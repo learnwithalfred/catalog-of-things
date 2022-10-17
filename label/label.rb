2e1d3ec912e3a02e9d5fa5c5bd9c4642f98edf32
Alfred Boateng <kwadwo.boateng.alfred@gmail.com>
2022-10-17 21:10:33 +0000

    Add lable class

---
+++ label/label.rb
@@ -0,0 +1,12 @@
+class Label
+  attr_accessor :title, :color
+  attr_reader :id, :items
+
+  def initialize(title, color, id = Random.rand(1..1000))
+    @title = title
+    @color = color
+    @id = id
+    @items = []
+  end
+
+end
