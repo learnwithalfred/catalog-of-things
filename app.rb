11ed8941aa984941d77e635030df1c32e9a7d3b9
Alfred Boateng <kwadwo.boateng.alfred@gmail.com>
2022-10-17 12:50:16 +0000

    Test app

--- app.rb
+++ app.rb
@@ -1,5 +1,3 @@
-require_relative 'options'
-
 class App
   def menu
     puts 'Welcome To the Catalog of things'
@@ -33,8 +31,42 @@ class App
     puts '13 - Exit'
   end
 
+  def select(choice)
+    case choice
+    when 1
+      puts 'List all books selected'
+    when 2
+      puts 'list_music_album selected'
+    when 3
+      puts 'list_movies selected '
+    when 4
+      puts 'list_games selected '
+    when 5
+      puts 'list_genres selected'
+    when 6
+      puts 'list_labels'
+    when 7
+      puts 'list_authors'
+    when 8
+      puts 'list_sources'
+    when 9
+      puts 'create_book selected'
+    when 10
+      puts 'create_music_album selected'
+    when 11
+      puts 'create_movie selected'
+    when 12
+      puts 'create_game selected'
+    when 13
+      puts 'Thanks For Using Our Program'
+      exit
+    else
+      puts "#{choice} Invalid option"
+    end
+  end
+
   def run
-    choice = 0
+    choice = 1
 
     while choice != 13
 
@@ -44,7 +76,8 @@ class App
 
       choice = gets.chomp.strip.to_i
 
-      selected(choice)
+
+      select(choice)
 
       print 'Press any key to continue....'
 
