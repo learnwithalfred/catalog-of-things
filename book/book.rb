87bda8bb46baa6725b4797160ae900e731917e5d
Alfred Boateng <kwadwo.boateng.alfred@gmail.com>
2022-10-17 21:12:24 +0000

    Create book class

---
+++ book/book.rb
@@ -0,0 +1,19 @@
+require './item'
+
+class Book < Item
+  attr_accessor :publisher, :cover_state, :publish_date, :title
+  attr_reader :id
+
+  def initialize(publisher, cover_state,
+                 publish_date,
+                 archived,
+                 label,
+                 id = Random.rand(1..1000))
+    super(publish_date, archived, id)
+    @publisher = publisher
+    @cover_state = cover_state
+    @id = id
+    @title = label
+  end
+
+end
