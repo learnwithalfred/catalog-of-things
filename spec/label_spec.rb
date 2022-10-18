cd6f57e2989ecd8ab09072417d46d0f1f98e1dad
Alfred Boateng <kwadwo.boateng.alfred@gmail.com>
2022-10-18 10:30:14 +0000

    Add test for label

---
+++ spec/label_spec.rb
@@ -0,0 +1,24 @@
+require './label/label'
+require './item'
+
+describe Label do
+  before :each do
+    @label = Label.new('Comedy', 'white')
+    @item = Item.new(2, "John Deo", "Kumerica", @label, "2022-01-01")
+    @label.add_item(@item)
+  end
+
+  describe '#new' do
+    it 'it should return an instance of Label object' do
+      expect(@label).to be_instance_of Label
+    end
+
+    it 'it should return an instance of Item object' do
+      expect(@item).to be_instance_of Item
+    end
+  end
+
+  it 'it should return one item' do
+    expect(@label.items.length).to eql 1
+  end
+end
