e8391aba1d02f2ca5b45eb2374265ead7acba916
Alfred Boateng <kwadwo.boateng.alfred@gmail.com>
2022-10-19 20:25:14 +0000

    Update test to pass

--- spec/games_spec.rb
+++ spec/games_spec.rb
@@ -1,10 +1,26 @@
-require 'rspec'
-require './game/games'
+require './game/game'
+require './item'
 
-RSpec.describe 'Game class' do
+describe 'Game class' do
   before :each do
-    @games = Game.new(true, '2022-10-12', '2022-12-12')
+    @games = Game.new('2022-12-12', true, '2022-10-12', "no", "Williams")
+    @fortnite = Game.new('2011-12-21', true, '2019-12-25', "no", 'john')
+    @fifa = Game.new('2020-04-21', true, '2022-04-25', "no", 'samuel')
   end
+    context 'Test for class Game' do
+    it 'should be an instance of class game' do
+      expect(@fortnite).to be_an_instance_of Game
+    end
+
+    it 'can_be_archived? return true' do
+      expect(@fortnite.can_be_archived?).to eq(true)
+    end
+
+    it 'can_be_archived? return false' do
+      expect(@fifa.can_be_archived?).to eq(false)
+    end
+  end
+
 
   describe 'Game class instance attributes' do
     it 'should return correct value for multiplayer' do
